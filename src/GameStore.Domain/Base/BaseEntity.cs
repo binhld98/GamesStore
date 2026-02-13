a97a99e22737e9c17ba230ac44050ca9b887e810
binhld <8991nofear@gmail.com>
2026-02-13 23:02:09 +0700

    feat: add base domain classes

---
+++ src/GameStore.Domain/Base/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿namespace GameStore.Domain.Base
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; protected set; }
+
+        private readonly List<BaseDomainEvent> _domainEvents = new();
+
+        public IReadOnlyCollection<BaseDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(BaseDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void RemoveDomainEvent(BaseDomainEvent domainEvent)
+        {
+            _domainEvents.Remove(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    }
+}
---
+++ src/GameStore.Domain/Base/BaseValueObject.cs
@@ -0,0 +1,29 @@
+﻿namespace GameStore.Domain.Base
+{
+    public abstract class BaseValueObject
+    {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return GetEqualityComponents().SequenceEqual(((BaseValueObject)obj).GetEqualityComponents());
+        }
+
+        override public int GetHashCode()
+        {
+            var hash = new HashCode();
+
+            foreach (var component in GetEqualityComponents())
+            {
+                hash.Add(component);
+            }
+
+            return hash.ToHashCode();
+        }
+    }
+}
